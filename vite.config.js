2b78b94157012d58568eaf84409b039520b55c62
simeunseo <rkdcjfehowl@naver.com>
2023-11-16 04:12:33 +0900

    init: svgr setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
+
+const __dirname = 'src';
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
+  resolve: {
+    alias: [{ find: '~', replacement: resolve(__dirname) }],
+  },
+});
