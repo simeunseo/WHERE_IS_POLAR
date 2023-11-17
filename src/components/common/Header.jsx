ac170fc3adda9a9b8befae96814e44d64f9d2df2
simeunseo <rkdcjfehowl@naver.com>
2023-11-17 22:57:46 +0900

    feat: header 추가

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,43 @@
+import LogoMain from '../../assets/svg/logo-main.svg?react';
+import styled from 'styled-components';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <LogoMain />
+      <Navigation>
+        <Category>인류의 꿈을 보관합니다</Category>
+        <Category>더 깊은 이야기</Category>
+        <Category>당신도 들려주세요</Category>
+      </Navigation>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+
+  padding: 5rem;
+`;
+
+const Navigation = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 5rem;
+  justify-content: end;
+`;
+
+const Category = styled.h1`
+  font-size: 3rem;
+
+  text-align: right;
+  /* 
+  &:hover {
+    &::before {
+      content: '*';
+    }
+  } */
+`;
--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,11 +1,21 @@
+import Constellation from '../assets/svg/constellation.svg?react';
+import Header from '../components/common/Header';
 import ItemList from '../components/archive/ItemList';
-
+import styled from 'styled-components';
 const Archive = () => {
   return (
-    <div>
+    <>
+      <Header />
       <ItemList />
-    </div>
+      <StyledConstellation />
+    </>
   );
 };
 
 export default Archive;
+
+const StyledConstellation = styled(Constellation)`
+  position: fixed;
+  top: 0;
+  z-index: -1;
+`;
