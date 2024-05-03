d58717c921163c8bdbc2f5fae5532de991085773
Eunseo Sim <55528304+simeunseo@users.noreply.github.com>
2024-05-03 20:59:11 +0900

    fix: router 변경

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -2,20 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Archive from './pages/Archive';
 import Gate from './pages/Gate';
-import Root from './pages/Root';
-import Yours from './pages/Yours';
 import Magazine from './pages/Magazine';
 import Magazine1 from './pages/Magazine1';
 import Magazine2 from './pages/Magazine2';
+import Root from './pages/Root';
+import Yours from './pages/Yours';
+
 // import ScrollToTop from './utils/scrollToTop';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Root />} />
+        <Route path="/logo" element={<Root />} />
         <Route path="/gate" element={<Gate />} />
-        <Route path="/archive" element={<Archive />} />
+        <Route path="/" element={<Archive />} />
         <Route path="/yours" element={<Yours />} />
         <Route path="/magazine" element={<Magazine />} />
         <Route path="/magazine/1" element={<Magazine1 />} />
